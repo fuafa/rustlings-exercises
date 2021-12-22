6fac416149625b30c9420f7ea6b4aeb59ecdddfe
allysleep <xiaofalzx@gmail.com>
2021-12-23 00:11:20 +0800

    de

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -5,12 +5,13 @@
 // For these exercises the code will fail to compile when there are clippy warnings
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
-
+use std::f64::EPSILON;
 
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    // if y != x {
+    if (y - x).abs() > EPSILON {
         println!("Success!");
     }
 }
